daa9369a6eb083f76cbc59e36604b29c5c19317a
Sultan Kadyrkesh <sultankadyr@outlook.com>
2019-04-16 15:25:11 +0600

    Редирект если не выполнен вход

--- src/components/anasayfa/Anasayfa.js
+++ src/components/anasayfa/Anasayfa.js
@@ -3,9 +3,14 @@ import PropTypes from 'prop-types';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { Redirect } from 'react-router-dom'
 import Anagrid from './Anagrid'
+import { withStyles } from '@material-ui/core';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
 
 class Anasayfa extends React.Component {
     render() {
+        const { auth } = this.props;
+        if (!auth.uid) return <Redirect to='/login' />
         return <React.Fragment>
             <CssBaseline />
             
@@ -14,4 +19,12 @@ class Anasayfa extends React.Component {
     }
 }
 
-export default Anasayfa;
+const mapStateToProps = (state) => {
+    return {
+        auth: state.firebase.auth,
+    }
+}
+
+export default compose(
+    connect(mapStateToProps)
+)(Anasayfa);
