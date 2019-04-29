daf1a8ea023c4b4ef0f533d59de8b929441de4fe
Sultan Kadyrkesh <sultankadyr@outlook.com>
2019-04-30 02:31:32 +0600

    About merge

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,20 +3,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
-import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Hidden from '@material-ui/core/Hidden';
-import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
 import Markdown from './Markdown';
 
+const aboutmd = `
+# Баймуратов Бекзат
+
+![Man](https://i.ibb.co/NTNmFNz/man.jpg)
+
+- Мастер спорта по боевому самбо
+- Кандидат мастера спорта по вольной борьбе
+- Кандидат мастера спорта по рукопашному бою
+- Основатель и тренер команды "EsilTopTeam" г.Есиль
+`
 
 const styles = theme => ({
   layout: {
@@ -78,51 +79,10 @@ const styles = theme => ({
   },
 });
 
-const sections = [
-  'Technology',
-  'Design',
-  'Culture',
-  'Business',
-  'Politics',
-  'Opinion',
-  'Science',
-  'Health',
-  'Style',
-  'Travel',
-];
-
-const featuredPosts = [
-  {
-    title: 'Featured post',
-    date: 'Nov 12',
-    description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-  },
-  {
-    title: 'Post title',
-    date: 'Nov 11',
-    description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-  },
-];
 
 
-const archives = [
-  'March 2020',
-  'February 2020',
-  'January 2020',
-  'December 2019',
-  'November 2019',
-  'October 2019',
-  'September 2019',
-  'August 2019',
-  'July 2019',
-  'June 2019',
-  'May 2019',
-  'April 2019',
-];
 
-const social = ['GitHub', 'Twitter', 'Facebook'];
+
 
 function About(props) {
   const { classes } = props;
@@ -137,35 +97,11 @@ function About(props) {
             {/* Main content */}
             <Grid item xs={12} md={12}>
               <Typography variant="h6" gutterBottom>
-                From the Firehose
+                О нас
               </Typography>
               <Divider />
                 <Markdown className={classes.markdown} >
-                  {`## Sample blog post
-
-#### April 1, 2020 by Olivier
-
-This blog post shows a few different types of content that are supported and styled with
-Material styles. Basic typography, images, and code are all supported.
-You can extend these by modifying
-
-Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.
-Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.
-
-Curabitur blandit tempus porttitor. **Nullam quis risus eget urna mollis** ornare vel eu leo.
-Nullam id dolor id nibh ultricies vehicula ut id elit.
-
-Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum.
-Aenean lacinia bibendum nulla sed consectetur.
-
-## Heading
-
-Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
-Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-
-### Sub-heading`}
+                  {aboutmd}
                 </Markdown>
               
             </Grid>
