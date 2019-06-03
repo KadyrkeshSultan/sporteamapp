9dd5f74f1c11cdd6c03660b0867765350198645f
Sultan Kadyrkesh <sultankadyr@outlook.com>
2019-06-03 19:09:13 +0600

    Кнопка выйти в настройках. Демо настройки

--- src/components/about/Settings.js
+++ src/components/about/Settings.js
@@ -1,36 +1,140 @@
 import React from 'react';
-import classNames from 'classnames';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-
+import { Redirect } from 'react-router-dom'
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AccountIcon from '@material-ui/icons/AccountCircle';
+import InfoIcon from '@material-ui/icons/Info';
+import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
+import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions';
+import ExitIcon from '@material-ui/icons/ExitToApp';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import {signOut} from '../../store/actions/authActions';
 
 const styles = theme => ({
-    icon: {
-        marginRight: theme.spacing.unit * 2,
-    },
-    layout: {
-        width: 'auto',
-        marginLeft: theme.spacing.unit * 3,
-        marginRight: theme.spacing.unit * 3,
-        [theme.breakpoints.up(500 + theme.spacing.unit * 3 * 2)]: {
-            width: '70%',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-        },
-    },
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: '33.33%',
+    flexShrink: 0,
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+  },
 });
 
 class Settings extends React.Component {
-    render() {
-        const { classes } = this.props;
+  state = {
+    expanded: 'panel3',
+  };
 
-        return <React.Fragment>
-            <CssBaseline />
-            <div className={classNames(classes.layout, classes.cardGrid)}>
-                
-                </div>
-        </React.Fragment>
+  handleChange = panel => (event, expanded) => {
+    this.setState({
+      expanded: expanded ? panel : false,
+    });
+  };
+
+  handleSignOut = () => {
+      this.props.signOut();
+  }
+
+  render() {
+    const { classes, auth } = this.props;
+    const { expanded } = this.state;
+
+    if (!auth.uid) return <Redirect to='/login' />
+
+    return (
+      <div className={classes.root}>
+              <Typography variant="h6" align="center" color="textSecondary" paragraph>
+                Настройки профиля
+            </Typography>
+        <ExpansionPanel expanded={expanded === 'panel1'} onChange={this.handleChange('panel1')}>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography className={classes.heading}><AccountIcon/> </Typography>
+            <Typography className={classes.secondaryHeading}>Общее</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              Тут будут настройки профиля
+            </Typography>
+          </ExpansionPanelDetails>
+          <Divider />
+            <ExpansionPanelActions>
+            <Button size="small">Отменить</Button>
+            <Button size="small" variant='outlined' color="primary">
+                Сохранить
+            </Button>
+            </ExpansionPanelActions>
+        </ExpansionPanel>
+        <ExpansionPanel expanded={expanded === 'panel2'} onChange={this.handleChange('panel2')}>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography className={classes.heading}><InfoIcon /> </Typography>
+            <Typography className={classes.secondaryHeading}>
+              Учетная запись
+            </Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              Тут будут настройки учетной записи
+            </Typography>
+          </ExpansionPanelDetails>
+          <Divider />
+            <ExpansionPanelActions>
+            <Button size="small">Отменить</Button>
+            <Button size="small" variant='outlined' color="primary">
+                Сохранить
+            </Button>
+            </ExpansionPanelActions>
+        </ExpansionPanel>
+        <ExpansionPanel expanded={expanded === 'panel3'} onChange={this.handleChange('panel3')}>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography className={classes.heading}><ExitIcon /> </Typography>
+            <Typography className={classes.secondaryHeading}>
+              Выйти из аккаунта
+            </Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails style={{justifyContent: 'center',}}>
+            <Typography>
+              <Button variant='outlined' onClick={this.handleSignOut}>Выйти</Button>
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+    return {
+      auth: state.firebase.auth,
+    }
+  }
+
+const mapDispatchToProps = (dispatch) => {
+    return{
+        signOut: () => dispatch(signOut())
     }
 }
 
-export default withStyles(styles)(Settings);
+  export default compose(
+    withStyles(styles),
+    connect(mapStateToProps, mapDispatchToProps),
+    // firestoreConnect(() => {
+    //     return [
+    //         { 
+    //             collection: 'users',
+    //         },
+    //     ]
+    // })
+)(Settings);
