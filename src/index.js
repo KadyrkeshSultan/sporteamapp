fcd83fff994449b95989f6ebc7127f32bf8e18a2
Sultan Kadyrkesh <sultankadyr@outlook.com>
2019-07-05 20:56:29 +0600

    pwa

--- src/index.js
+++ src/index.js
@@ -22,7 +22,6 @@ const store = createStore(rootReducer,
 
 store.firebaseAuthIsReady.then(() => {
     ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-    registerServiceWorker();
 })
 
 
